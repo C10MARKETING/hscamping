9d0f856736b790e309f6c06c7b726b649c19c4f7
C10MARKETING <c10@c10marketing.dk>
2017-06-29 17:48:17 +0200

    Opdateret gæstetyper m.m.
    Tilføjet mere intuitive knapper ved tilmeld/frameld og
    interesseret/ikke, tilføjet ny gæsttype og tilføjet max deltagere på
    aktiviteter i appen

--- app/src/providers/notifications.service.ts
+++ app/src/providers/notifications.service.ts
@@ -9,45 +9,53 @@ import * as moment from 'moment'
 @Injectable()
 export class NotificationsService {
 
-  activityIn: string;
-  minutes: string;
+  activityIn: string = "Aktivitet starter om";
+  minutes: string = "minutter";
 
   constructor(private translate: TranslateService, private app: App, private LocalNotifications: LocalNotifications, private platform: Platform, private alertCtrl: AlertController) {
 
+    if (platform.is('cordova')) {
+
+      this.LocalNotifications.registerPermission().then(() => {
+
+        LocalNotifications.on('click', (event) => {
+          console.log(event)
+          let data = JSON.parse(event.data)
+          this.openItem(data.aid) 
+        });
+
+        LocalNotifications.on('trigger', function (notification) {
+          console.log(notification);
+          //to handle notification popup in foreground.
+          let alert = alertCtrl.create({
+              message: notification.title + ". " + notification.text + ".",
+              buttons: [{ text: "Ok", role: 'cancel' } ]
+          });
+          alert.present();
+        });
+        var that = this;
+        setTimeout(function() { that.setupLocalizedText();}, 1000);
+
+      });
+    }
+  }
+
+  setupLocalizedText(){
     this.translate.get('NOTIFICATIONS_ACTIVITYSTARTSIN').subscribe(
       value => { this.activityIn = value; })
 
     this.translate.get('NOTIFICATIONS_MINUTES').subscribe(
       value => { this.minutes = value; })
+  }
 
-    if (platform.is('cordova')) {
-
-      LocalNotifications.registerPermission();
-
-      LocalNotifications.on('click', (event) => {
-        console.log(event)
-        let data = JSON.parse(event.data)
-        this.openItem(data.aid) 
-      });
-
-/*      LocalNotifications.on('trigger', function (notification) {
-        console.log(notification);
-        //to handle notification popup in foreground.
-        let alert = alertCtrl.create({
-            message: notification.title + ". " + notification.text + ".",
-            buttons: [{ text: "Ok", role: 'cancel' } ]
-        });
-        alert.present();
-      });
-
-      LocalNotifications.schedule({
-        id: 1337,
-        at: moment().valueOf() + 2000,
-        title: "davs",
-        text: "tekst",
-        data: { aid: 32, startsIn: 33 }
-      });*/
-    }
+  testNotification(){
+    this.LocalNotifications.schedule({
+      id: 1337,
+      at: moment().valueOf() + 1000,
+      title: "davs",
+      text: "tekst: " + this.activityIn + " 42 " + this.minutes,
+      data: { aid: 32, startsIn: 33 }
+    });
   }
 
   deleteNotification(activity) {
